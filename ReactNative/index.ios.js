1742d95d9daa88588b71c5f10a1b810d1cf9d4fe
jiafa <18321274679@163.com>
2016-08-25 09:33:32 +0800

    Add RN files
    Add RN files

--- ReactNative/index.ios.js
+++ ReactNative/index.ios.js
@@ -1,21 +1,32 @@
-'use strict';
+/**
+ * Created by ndmac on 16/8/23.
+ */
+
 import React, { Component } from 'react';
 import { StyleSheet , View ,AppRegistry ,Text ,Animated, Easing, Image} from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import DemoOne from './src/DemoOne'
 
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
-class RNComponent extends Component {
+export default class Demo extends Component {
     render() {
         return (
-            <View style={styles.container}>
-            </View>);
+                <Provider store={store}>
+                <DemoOne />
+                </Provider>
+                );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor:'gray',
-        flex:1
-    }
-});
+                                 container: {
+                                 backgroundColor:'gray',
+                                 flex:1
+                                 }
+                                 });
 
-AppRegistry.registerComponent('RNModuleName',()=> RNComponent);
+AppRegistry.registerComponent('RNModuleName',()=> Demo);
